30a117aa18a086f83415c848cb7e264dc9fa61ba
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2025-01-25 12:47:28 -0800

    Finished

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -27,22 +27,20 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
+
     public int findFirstVowel(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0.
-        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
+        for (int i = 0; i < sWord.length(); i++) {
+            char c = sWord.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return i;
+        }
         return -1;
     }
 
     public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
+        int vowelAt = findFirstVowel(sWord);
+        if(vowelAt == -1) return sWord + "ay";
+        else if (vowelAt == 0) return sWord + "way";
+        else if (sWord.substring(0,2).equals("qu")) return sWord.substring(2) + "quay";
+        else return sWord.substring(vowelAt) + sWord.substring(0, vowelAt) + "ay";
     }
-}//end PigLatin class
+}// end PigLatin class
